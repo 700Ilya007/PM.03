f3ecf0540cf313006245279b0f7e94209aa2ce91
700Ilya007 <95218573+700Ilya007@users.noreply.github.com>
2023-04-26 11:00:28 +0300

    Констуктор класса

---
+++ Src/Exam/Exam/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+
+    public class Fish
+    {
+        public string FishType { get; set; }
+        public string Manufacturer { get; set; }
+        public string Price { get; set; }
+
+        //конструктор класса
+        public Fish(string fishType, string manufacturer, string price)
+        {
+            FishType = fishType;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+    }
+
+   
+}
